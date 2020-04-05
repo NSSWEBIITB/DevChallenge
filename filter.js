726d05b0cdfa507b6fabaa78704c6214acd71084
ShubhamKaudewar <kaudewar@gmail.com>
2020-04-05 13:39:23 +0530

    3FiltersAdded

--- filter.js
+++ filter.js
@@ -3,9 +3,35 @@ sels = document.getElementsByTagName('select');
  {
   sels[i].addEventListener('change', function() {
      selectionID = this.id;
-     if(selectionID === "filter")
+    //  Profficiency Filter
+     if(selectionID === "filter1")
      {
-         var selected = document.getElementById("filter").value
+         var selected = document.getElementById("filter1").value
+         var cur_items = document.getElementById('data-item').getElementsByClassName('span');
+         
+         $.getJSON("data.json",function(data){
+            $.each(data, function(key,value){
+                if( value.id === cur_items[i].innerText){
+                    for(var j = 0; j < 2; j++)
+                    {
+                        if(selected == "All" || value.role == selected || value.level == selected )
+                        {
+                            document.getElementById("data-item").style.display = "block";
+                        }
+                        else
+                        {
+                            document.getElementById("data-item").style.display = "none"; 
+                        }
+                    }
+                }
+            });
+        });
+     }
+
+     //Language Filter
+     if(selectionID === "filter2")
+     {
+         var selected = document.getElementById("filter2").value
          var cur_items = document.getElementById('data-item').getElementsByClassName('span');
          
          $.getJSON("data.json",function(data){
@@ -14,17 +40,39 @@ sels = document.getElementsByTagName('select');
                     for(var j = 0; j < (languages.length+2); j++)
                     {
                         for (x in value.languages) {
-                            if (value.languages[x] == selected) {
+                            if (selected == "All" || value.languages[x] == selected) {
                                 document.getElementById("data-item").style.display = "block";
                             }
+                            else
+                            {
+                                document.getElementById("data-item").style.display = "none"; 
+                            }
                         }
-                        if(selected == "All" || value.role == selected || value.level == selected )
-                        {
-                            document.getElementById("data-item").style.display = "block";
-                        }
-                        else
-                        {
-                            document.getElementById("data-item").style.display = "none"; 
+                    }
+                }
+            });
+        });
+     }
+
+     //Tools Filter
+     if(selectionID === "filter3")
+     {
+         var selected = document.getElementById("filter3").value
+         var cur_items = document.getElementById('data-item').getElementsByClassName('span');
+         
+         $.getJSON("data.json",function(data){
+            $.each(data, function(key,value){
+                if( value.id === cur_items[i].innerText){
+                    for(var j = 0; j < (tools.length); j++)
+                    {
+                        for (x in value.tools) {
+                            if (selected == "All" || value.tools[x] == selected) {
+                                document.getElementById("data-item").style.display = "block";
+                            }
+                            else
+                            {
+                                document.getElementById("data-item").style.display = "none"; 
+                            }
                         }
                     }
                 }
